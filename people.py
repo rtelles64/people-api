036942bb0816322d8aec2c217785e9b368a54707
Roy Telles <rtelles64@gmail.com>
2024-06-03 15:01:14 -0700

    Add: create method

--- people.py
+++ people.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import abort
 
 
 def get_timestamp():
@@ -26,3 +27,21 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname], 201
+    else:
+        abort(
+            406,
+            f"Person with last name {lname} already exists",
+        )
