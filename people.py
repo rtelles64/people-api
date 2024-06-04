f162970e6a4bb4dbabbaa154d1a6a6320372d930
Roy Telles <rtelles64@gmail.com>
2024-06-04 10:35:30 -0700

    Add: read_one, update, delete functions

--- people.py
+++ people.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import abort
+from flask import abort, make_response
 
 
 def get_timestamp():
@@ -44,4 +44,39 @@ def create(person):
         abort(
             406,
             f"Person with last name {lname} already exists",
+        )
+
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
         )
