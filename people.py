fddd4d31704ab4534321951cba8f0b7190bed2fc
Roy Telles <rtelles64@gmail.com>
2024-06-10 14:44:27 -0700

    Update: Refactor logic to use new classes

--- people.py
+++ people.py
@@ -1,82 +1,55 @@
-from datetime import datetime
 from flask import abort, make_response
 
-
-def get_timestamp():
-    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-
-
-PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    }
-}
-
-
-def read_all():
-    return list(PEOPLE.values())
+from config import db
+from models import Person, people_schema, person_schema, session
 
 
 def create(person):
     lname = person.get("lname")
-    fname = person.get("fname", "")
+    existing_person = session.query(Person).filter_by(lname=lname).one_or_none()
 
-    if lname and lname not in PEOPLE:
-        PEOPLE[lname] = {
-            "lname": lname,
-            "fname": fname,
-            "timestamp": get_timestamp(),
-        }
-        return PEOPLE[lname], 201
+    if existing_person is None:
+        new_person = person_schema.load(person, session=session)
+        session.add(new_person)
+        session.commit()
+        return person_schema.dump(new_person), 201
     else:
-        abort(
-            406,
-            f"Person with last name {lname} already exists",
-        )
+        abort(406, f"Person with last name {lname} already exists")
+
+
+def read_all():
+    people = session.query(Person).all()
+    return people_schema.dump(people)
 
 
 def read_one(lname):
-    if lname in PEOPLE:
-        return PEOPLE[lname]
+    person = session.query(Person).filter_by(lname=lname).one_or_none()
+
+    if person is not None:
+        return person_schema.dump(person)
     else:
-        abort(
-            404,
-            f"Person with last name {lname} not found"
-        )
+        abort(404, f"Person with last name {lname} not found")
 
 
 def update(lname, person):
-    if lname in PEOPLE:
-        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
-        PEOPLE[lname]["timestamp"] = get_timestamp()
-        return PEOPLE[lname]
+    existing_person = session.query(Person).filter_by(lname=lname).one_or_none()
+
+    if existing_person:
+        updated_person = person_schema.load(person, session=session)
+        existing_person.fname = updated_person.fname
+        session.merge(existing_person)
+        session.commit()
+        return person_schema.dump(existing_person), 201
     else:
-        abort(
-            404,
-            f"Person with last name {lname} not found"
-        )
+        abort(404, f"Person with last name {lname} not found")
 
 
 def delete(lname):
-    if lname in PEOPLE:
-        del PEOPLE[lname]
-        return make_response(
-            f"{lname} successfully deleted", 200
-        )
+    existing_person = session.query(Person).filter_by(lname=lname).one_or_none()
+
+    if existing_person:
+        session.delete(existing_person)
+        session.commit()
+        return make_response(f"{lname} successfully deleted", 200)
     else:
-        abort(
-            404,
-            f"Person with last name {lname} not found"
-        )
+        abort(404, f"Person with last name {lname} not found")
