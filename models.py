f9d50b224eee2deeafee8db01d42403dc210c76c
Roy Telles <rtelles64@gmail.com>
2024-06-11 11:49:43 -0700

    Update: models to include relationship between Note and Person; Add: Note class

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from config import db
 from datetime import datetime
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 # Due to version conflicts, the project will use pure sqlalchemy modules on its
 # own, and not the flask_sqlalchemy extension.
@@ -11,12 +11,45 @@ Session = sessionmaker(bind=db.engine)
 session = Session()
 
 
+class Note(Base):
+    __tablename__ = "note"
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey("person.id"))
+    content = Column(String, nullable=False)
+    timestamp = Column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+
 class Person(Base):
     __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
     lname = Column(String(32), unique=True)
     fname = Column(String(32))
     timestamp = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    # Define relationship to a Note
+    notes = relationship(
+        # This could either be a string or a class, but a string is easier as
+        # the editor won't throw any errors.
+        'Note',
+        # Each instance of Note will contain an attribute called .person. This
+        # attribute references the parent object that a particular Note instance
+        # is related to.
+        backref='person',
+        # This parameter determines how to treat Note instances when changes are
+        # made to the parent Person instance. For example, if a Person objected
+        # is deleted, all of its associated Notes will be deleted as well.
+        cascade='all, delete, delete-orphan',
+        # This is required if 'delete-orphan' is part of the cascade parameter.
+        # This tells SQLAlchemy not to allow an orphaned Note instance to exist
+        # because each Note has a single parent.
+        single_parent=True,
+        # This tells SQLAlchemy how to sort the Note instances associated with
+        # a Person object. When a Person is retrieved, by default the notes
+        # attribute list will be in an unknown order. This ensures the notes
+        # get returned in descending order, from newest to oldest.
+        order_by='desc(Note.timestamp)'
+    )
 
 
 class PersonSchema(SQLAlchemyAutoSchema):
